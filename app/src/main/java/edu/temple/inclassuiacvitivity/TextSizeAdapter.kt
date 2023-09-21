e2b0b599378924ef61bb0ad5051addc2db7348f1
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2023-09-21 14:49:23 -0400

    implimented four function, getCount, getItem, getId....

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,17 +1,23 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter(private val textSizes: Array<Int>) : BaseAdapter () {
+class TextSizeAdapter(private val context: Context, private val textSizes: Array<Int>) : BaseAdapter () {
 
     override fun getCount()= textSizes.size
     override fun getItem(p0: Int) = textSizes[p0]
     override fun getItemId(p0: Int) = p0.toLong()
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = textSizes[p0].toString()
+        textView.textSize = textSizes[p0].toFloat()
+        textView.setPadding(5, 10,0,10)
+     return textView
     }
 
 }
